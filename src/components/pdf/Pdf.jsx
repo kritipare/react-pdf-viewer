76e28cfde31515476ea8dcbd1b49be4102461d67
“Kriti <kritipare308@gmail.com>
2021-06-06 22:22:01 +0530

    first page showing

--- src/components/pdf/Pdf.jsx
+++ src/components/pdf/Pdf.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf';
+// import samplePDF from './git-cheat-sheet-education.pdf';
 
 const Pdf = () => {
+  const [numPages, setNumPages] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+    setCurrentPage(1);
+  };
+
   return (
     <div>
-      The Pdf will appear here
+      <Document
+        file='https://education.github.com/git-cheat-sheet-education.pdf'
+        // options={{ workerSrc: "/pdf.worker.js" }}
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page pageNumber={currentPage} />
+      </Document>
+      Page {currentPage || '--'} of {numPages || '--'}
     </div>
   )
 }
